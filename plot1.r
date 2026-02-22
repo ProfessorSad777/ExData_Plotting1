3ff12f8914e93b1fcaa294a42c0e32c4108bd430
ProfessorSad777 <saathvikachar84@gmail.com>
2026-02-23 01:08:29 +0530

    Clean repo without dataset

---
+++ plot1.r
@@ -0,0 +1,5 @@
+
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", dec = ".", na.strings = "?")
+data <- data[data$Date %in% c("1/2/2007","2/2/2007"), ]
+hist(data[, 3], col = "red", main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)")
